a4c589b398071d46325ef4159a388a07d90fb0a6
P00po <dulustan1996@gmail.com>
2021-11-05 16:11:32 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+import * as THREE from 'three';
+
+			import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
+
+			let container, clock;
+			let camera, scene, renderer, elf;
+
+			init();
+			animate();
+
+			function init() {
+
+				container = document.getElementById( 'container' );
+
+				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000 );
+				camera.position.set( 8, 10, 8 );
+				camera.lookAt( 0, 3, 0 );
+
+				scene = new THREE.Scene();
+
+				clock = new THREE.Clock();
+
+				// loading manager
+
+				const loadingManager = new THREE.LoadingManager( function () {
+
+					scene.add( elf );
+
+				} );
+
+				// collada
+
+				const loader = new ColladaLoader( loadingManager );
+				loader.load( './lol.dae', function ( collada ) {
+
+					elf = collada.scene;
+          elf.position.set(0, 3, 0);
+				} );
+
+				//
+
+				const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+				scene.add( ambientLight );
+
+				const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+				directionalLight.position.set( 1, 1, 0 ).normalize();
+				scene.add( directionalLight );
+
+				//
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				//
+
+        // positon the 3d model
+        
+				//
+
+				window.addEventListener( 'resize', onWindowResize );
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+
+			}
+
+			function render() {
+
+				const delta = clock.getDelta();
+
+				if ( elf !== undefined ) {
+
+					elf.rotation.z += delta * 0.5;
+
+				}
+
+				renderer.render( scene, camera );
+
+			}
